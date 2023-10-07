3e58e53e1c351b49461a984dc0bfd3e63d50d698
Jeetu Gupta <jeetu270602@gmail.com>
2023-10-07 19:00:53 +0530

    Create navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,23 +1,35 @@
 import { Outlet, Link } from "react-router-dom";
+import cartIcon from '../assets/cart-icon.svg'
+import "../assets/styles/NavBar.css";
 
 export default function NavBar(){
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+      <nav className="main-nav">
+        <h1 className="logo">
+          <Link to="/" className="nav-link">
+            T-Corp
+          </Link>
+        </h1>
+        <ul className="nav-list">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
           </li>
-          <li>
-            <Link to="/product">Product</Link>
-          </li>
-          <li>
-            <Link to="/cart">Cart</Link>
+          <li className="nav-item">
+            <Link to="/product" className="nav-link">
+              Product
+            </Link>
           </li>
         </ul>
+        <Link to="/cart" className="cart-link">
+          <img src={cartIcon} alt="cart image" className="cart-icon" />
+          Cart(10)
+        </Link>
       </nav>
 
       <Outlet />
     </>
-  )
+  );
 };
